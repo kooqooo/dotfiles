ae6af2118b74fd3fd409ad64a6ad61a6f87a2756
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-01-01 22:18:36 +0900

    feat: hangul.sh
    * 한글 사용이 가능하도록 설정

---
+++ hangul.sh
@@ -0,0 +1,19 @@
+# 패키지 설치
+sudo apt-get install language-pack-ko
+
+# 로케일 설치
+sudo locale-gen ko_KR.UTF-8
+
+# 패키지 재성정
+sudo dpkg-reconfigure locales
+
+# 환경변수 설정
+export LANGUAGE=ko_KR.UTF-8
+export LANG=ko_KR.UTF-8
+
+# shrc에 추가하기
+if [ -f ~/.zshrc ]; then
+    echo -e "export LANGUAGE=ko_KR.UTF-8\nexport LANG=ko_KR.UTF-8" >> ~/.zshrc
+else
+    echo -e "export LANGUAGE=ko_KR.UTF-8\nexport LANG=ko_KR.UTF-8" >> ~/.bashrc"
+fi
