15b65b32027248eb0385ece2d3b8c932bbef3ced
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-02-29 03:35:35 +0900

    Create ubuntu_python.sh

---
+++ ubuntu_python.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+sudo apt install python3.11
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11
+sudo update-alternatives --config python3
+sudo apt install python-is-python3
