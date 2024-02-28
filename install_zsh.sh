0b7c0bda78255c56ba74c9febb15549879024025
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-02-29 03:29:38 +0900

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,5 +1,5 @@
 # Install zsh
-sudo apt-get install -y zsh
+sudo apt install -y zsh
 sudo chsh -s /bin/zsh "$USER"
 
 # Install oh-my-zsh
@@ -13,17 +13,33 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # # Install spaceship theme
-# if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt ]; then
-#     git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
-# fi
-# ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt ]; then
+    git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
+fi
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
+sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+ORIGINAL="plugins=(git)"
+NEW="plugins=(git\n\t\ttmux\n\t\tzsh-autosuggestions\n\t\tzsh-syntax-highlighting)"
+bash -c sed -i ":a;N;\$!ba;s/$ORIGINAL/$NEW/g" ~/.zshrc
+
+cat << EOF >> ~/.zshrc
+
+prompt_end() {
+  if [[ -n \$CURRENT_BG ]]; then
+    echo -n " %{%k%F{\$CURRENT_BG}%}\$SEGMENT_SEPARATOR"
+  else
+    echo -n "%{%k%}"
+  fi
+  echo -n "\n%{%F{\$CURRENT_BG}%}\$SEGMENT_SEPARATOR%{%f%}"
+  CURRENT_BG=''
+}
+EOF
 
 # add alias
-echo 'alias ns="watch -n .2 nvidia-smi"' >> ~/.zshrc
+echo 'alias wa="watch -n .2 nvidia-smi"' >> ~/.zshrc
 echo 'alias vz="vim ~/.zshrc"' >> ~/.zshrc
 echo 'alias sz="source ~/.zshrc"' >> ~/.zshrc
-echo 'alias act="source activate base"' >> ~/.zshrc
 source ~/.zshrc
 
 # Copy .zshrc
