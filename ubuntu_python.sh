034f7c2b9c92ff500ee6cc785a8da36eb25dd2fd
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-03-07 17:47:01 +0900

    fix: install pip

--- ubuntu_python.sh
+++ ubuntu_python.sh
@@ -3,7 +3,9 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt install -y python$PYTHON_VER
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-sudo apt install python3-pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+rm get-pip.py
 sudo apt install python-is-python3
 
 # sudo update-alternatives --config python3
