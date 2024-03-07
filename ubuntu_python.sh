be49ef075d683ac10d321b8905296e344adf1e45
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-03-07 17:42:29 +0900

    fix: set python3=python3.10

--- ubuntu_python.sh
+++ ubuntu_python.sh
@@ -2,8 +2,10 @@ PYTHON_VER="3.10"
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt install -y python$PYTHON_VER
-sudo ln -sf /usr/bin/python$PYTHON_VER /usr/bin/python
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo apt install python3-pip
+sudo apt install python-is-python3
 
-# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10
 # sudo update-alternatives --config python3
-# sudo apt install python-is-python3
+
+# sudo ln -sf /usr/bin/python$PYTHON_VER /usr/bin/python
