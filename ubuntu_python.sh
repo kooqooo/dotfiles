313a1b08415a516fcf60d541a2ca3105c957da92
구희찬 <67735022+kooqooo@users.noreply.github.com>
2024-03-16 15:55:59 +0900

    fix: link python version
    파이썬 버전을 새로 설치한 버전으로 링크하는 과정에서 기본 패키지 의존성 문제가 발생하므로 이 부분을 주석처리 하였습니다.

--- ubuntu_python.sh
+++ ubuntu_python.sh
@@ -2,7 +2,7 @@ PYTHON_VER="3.10"
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt install -y python$PYTHON_VER
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm get-pip.py
