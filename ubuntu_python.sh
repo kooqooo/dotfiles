4815f9fbd0c304593971b1d239ec9b97baaf19fa
구희찬 <koogmlcks@gmail.com>
2024-05-31 15:52:02 +0900

    Update ubuntu_python.sh

--- ubuntu_python.sh
+++ ubuntu_python.sh
@@ -1,10 +1,10 @@
-ORIGIN_PYTHON_VER="3.8"
+EXISTING_PYTHON_VER="3.8"
 PYTHON_VER="3.10"
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt install -y python$PYTHON_VER
 
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$ORIGIN_PYTHON_VER 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$EXISTING_PYTHON_VER 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$PYTHON_VER 2
 sudo update-alternatives --config python3
 
