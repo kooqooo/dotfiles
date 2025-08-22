6f520beb7e500bc6b1ddf9ec2e9780d789bc6874
구희찬 <koogmlcks@gmail.com>
2025-08-22 19:12:10 +0900

    Update ubuntu_python.sh

--- ubuntu_python.sh
+++ ubuntu_python.sh
@@ -1,12 +1,7 @@
-EXISTING_PYTHON_VER="3.8"
-PYTHON_VER="3.10"
+PYTHON_VER="3.13"
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-apt-get update
+apt update
 sudo apt install -y python$PYTHON_VER
-
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$EXISTING_PYTHON_VER 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$PYTHON_VER 2
-sudo update-alternatives --config python3
 
 sudo apt-get install --reinstall -y python3-apt
 
@@ -15,8 +10,4 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python$PYTHON_VER get-pip.py
 rm get-pip.py
 
-sudo update-alternatives --install /usr/bin/pip pip /usr/local/bin/pip$PYTHON_VER 1
-sudo update-alternatives --config pip
 pip --version
-
-# sudo ln -sf /usr/bin/python$PYTHON_VER /usr/bin/python
